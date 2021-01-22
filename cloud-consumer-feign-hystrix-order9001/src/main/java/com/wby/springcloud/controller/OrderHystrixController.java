e957bd2986388feb070337b5d1157912a6cf4808
justtimo <wby6225104@163.com>
2021-01-23 01:50:56 +0800

    Hystix：代码膨胀。使用全局降级方法

--- cloud-consumer-feign-hystrix-order9001/src/main/java/com/wby/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order9001/src/main/java/com/wby/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.wby.springcloud.controller;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.wby.springcloud.service.PaymentHystrixService;
@@ -13,6 +14,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "globalMethodHandler")//设置全局降级方法
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -30,6 +32,7 @@ public class OrderHystrixController {
     })
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        //int i = 1/0; //模拟消费者出现异常，走兜底方法
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         log.info("*******result:"+result);
         return result;
@@ -38,6 +41,12 @@ public class OrderHystrixController {
     //兜底方法，上面方法出问题,我来处理，返回一个出错信息
     public String payment_TimeoutHandler(@PathVariable("id") Integer id) {
         return "我是消费者9001,对方支付系统繁忙请10秒后再试。或自己运行出错，请检查自己。";
+    }
+
+
+    //统一降级处理的默认方法
+    public String globalMethodHandler(){
+        return "使用全局降级方法";
     }
 
 
