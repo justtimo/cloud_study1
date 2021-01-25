dbce67743a5d5b099f2728f943f21277dedf46ea
justtimo <wby6225104@163.com>
2021-01-26 03:25:08 +0800

    GetWay:自定义过滤器

---
+++ cloud-gateway-gateway9527/src/test/java/com/wby/springclou/test/TestTime.java
@@ -0,0 +1,11 @@
+package com.wby.springclou.test;
+
+import java.time.ZonedDateTime;
+
+public class TestTime {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+
+    }
+}
