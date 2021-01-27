600128946565f559831b5f7ffb11138659c36811
justtimo <wby6225104@163.com>
2021-01-27 10:35:47 +0800

    sentinel：@SentinelResource

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/wby/springcloud/controller/RateLimitController.java
@@ -0,0 +1,26 @@
+package com.wby.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.wby.springcloud.bean.CommonResult;
+import com.wby.springcloud.bean.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RateLimitController{
+
+    /**
+     * 1.通过value资源名称进行限流，就会执行blockHandler指定的降级方法
+     * 2.通过uri路径进行限流，不执行blockHandler指定的降级方法
+     * @return
+     */
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource111",blockHandler = "handleException")
+    public CommonResult byResource(){
+        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
+    }
+    public CommonResult handleException(BlockException exception){
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+}
